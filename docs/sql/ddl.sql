d16332ce1a3545377cc65ad005af34bf8b57e096
Curtis Chavez <cchavez539@cnm.edu>
2020-06-29 13:18:33 -0600

    Added sql.

---
+++ docs/sql/ddl.sql
@@ -0,0 +1,17 @@
+create table if not exists `Source`
+(
+    `source_id` integer primary key autoincrement not null,
+    `name`      text                              not null collate NOCASE
+);
+
+create unique index if not exists `index_Source_name` on `Source` (`name`);
+
+create table if not exists `Quote`
+(
+    `quote_id`  integer primary key autoincrement not null,
+    `source_id` integer,
+    `text`      text                              not null collate NOCASE,
+    foreign key (`source_id`) references `Source` (`source_id`) on update no action on delete set null
+);
+
+create index if not exists `index_Quote_source_id` on `Quote` (`source_id`);

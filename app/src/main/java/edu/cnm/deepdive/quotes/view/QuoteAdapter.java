956223332d15a278899687997444035f12fbdb58
Curtis Chavez <cchavez539@cnm.edu>
2020-06-30 07:56:33 -0600

    Added source and text fields.

--- app/src/main/java/edu/cnm/deepdive/quotes/view/QuoteAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/quotes/view/QuoteAdapter.java
@@ -15,17 +15,20 @@ import java.util.List;
 
 public class QuoteAdapter extends RecyclerView.Adapter<Holder> {
 
-  private final String unnattributedSource;
+  private final String unattributedSource;
   private final String sourceFormat;
   private final Context context;
   private final List<QuoteWithSource> quotes;
+  private final OnClickListener clickListener;
 
-  public QuoteAdapter(Context context, List<QuoteWithSource> quotes) {
+  public QuoteAdapter(Context context, List<QuoteWithSource> quotes,
+      OnClickListener clickListener) {
     super();
     this.context = context;
     this.quotes = quotes;
-    unnattributedSource = context.getString(R.string.unattributed_source);
+    unattributedSource = context.getString(R.string.unattributed_source);
     sourceFormat = context.getString(R.string.source_format);
+    this.clickListener = clickListener;
   }
 
   @NonNull
@@ -47,11 +50,13 @@ public class QuoteAdapter extends RecyclerView.Adapter<Holder> {
 
   class Holder extends RecyclerView.ViewHolder {
 
+    private final View itemView;
     private final TextView quote;
     private final TextView source;
 
     public Holder(@NonNull View itemView) {
       super(itemView);
+      this.itemView = itemView;
       quote = itemView.findViewById(R.id.quote);
       source = itemView.findViewById(R.id.source);
     }
@@ -59,10 +64,17 @@ public class QuoteAdapter extends RecyclerView.Adapter<Holder> {
     private void bind(int position) {
       QuoteWithSource item = quotes.get(position);
       String sourceName =
-          (item.getSource() != null) ? item.getSource().getName() : unnattributedSource;
+          (item.getSource() != null) ? item.getSource().getName() : unattributedSource;
       quote.setText(item.getText());
       source.setText(String.format(sourceFormat, sourceName));
+      itemView.setOnClickListener((v) -> clickListener.onClick(v, getAdapterPosition(), item));
     }
+
+  }
+
+  public interface OnClickListener {
+
+    void onClick(View v, int position, QuoteWithSource quote);
 
   }
 
